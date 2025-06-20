451861bcdf87150223c582ecb3407f04e199e08f
GameBoy1979 <mth19794u@gmail.com>
2025-06-21 00:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from rembg import remove
 from PIL import Image
 import io
 import zipfile
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -28,10 +29,5 @@ def remove_background():
     return send_file(zip_buffer, as_attachment=True, download_name='output.zip', mimetype='application/zip')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    import os
-
-if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
